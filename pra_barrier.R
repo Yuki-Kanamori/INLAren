ed6600eb2d27231d7d3596eeedb85fc7fffeb69b
Yuki-Kanamori <kana.yuki@fra.affrc.go.jp>
2020-07-14 15:29:10 +0900

    initial commit

---
+++ pra_barrier.R
@@ -0,0 +1,62 @@
+library(scales)
+library(rgeos)
+## High resolution maps when using map()
+library(mapdata) 
+## Map features, map2SpatialPolygons()
+library(maptools)
+
+# Select region 
+map <- map("world", "Japan", fill = TRUE,
+           col = "transparent", plot = TRUE)
+IDs <- sapply(strsplit(map$names, ":"), function(x) x[1])
+map.sp <- map2SpatialPolygons(
+  map, IDs = IDs,
+  proj4string = CRS("+proj=longlat +datum=WGS84"))
+plot(map.sp)
+summary(map.sp)
+
+# make a polygon ------------------------------------------------------------
+pl.sel <- SpatialPolygons(list(Polygons(list(Polygon(
+  cbind(c(139.7, 139.5, 139.7, 140.1, 140.3, 139.9), # x-axis 
+        c(35.2,  35.4,  35.8,  35.8,  35.4,  35.2)), # y-axis
+  FALSE)), '0')), proj4string = CRS(proj4string(map.sp)))
+poly.water <- gDifference(pl.sel, map.sp)
+plot(pl.sel)
+plot(map.sp)
+plot(poly.water)
+
+## ------------------------------------------------------------------------
+# Define UTM projection
+kmproj <- CRS("+proj=utm +zone=53 ellps=WGS84 +units=km")
+# Project data
+poly.water = spTransform(poly.water, kmproj)
+pl.sel = spTransform(pl.sel, kmproj)
+map.sp = spTransform(map.sp, kmproj)
+
+## ------------------------------------------------------------------------
+max.edge = 3
+bound.outer = 5
+mesh <- inla.mesh.2d(boundary = poly.water,
+                     max.edge = c(1, 2) * max.edge,
+                     cutoff = 2,
+                     offset = c(max.edge, bound.outer))
+plot(mesh)
+
+
+## ------------------------------------------------------------------------
+water.tri = inla.over_sp_mesh(poly.water, y = mesh, 
+                              type = "centroid", ignore.CRS = TRUE)
+num.tri = length(mesh$graph$tv[, 1])
+barrier.tri = setdiff(1:num.tri, water.tri)
+poly.barrier = inla.barrier.polygon(mesh, 
+                                    barrier.triangles = barrier.tri)
+plot(poly.barrier)
+
+
+## ----label = "plot-barr-mesh2", fig = TRUE, echo = FALSE, fig.align = "center", fig.width = 6, heigh = 4.5, width = '97%', fig.cap = "The mesh constructed both over water and land. The grey region is the original land map. The inner red outline marks the coastline barrier."----
+
+plot(mesh, lwd = 0.5, add = FALSE)
+plot(pl.sel, add = TRUE)
+plot(map.sp, add = TRUE, col = gray(.9))
+plot(poly.barrier, border = "red", add = TRUE)
+
