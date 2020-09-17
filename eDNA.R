e571bce1b0292a24cccb00bf7172a3171d395ade
Yuki-Kanamori <y.kanamory@gmail.com>
2020-09-17 09:58:54 +0900

    checked the toy data

--- eDNA.R
+++ eDNA.R
@@ -131,7 +131,7 @@ w1 = c(rep(NA, n), rep(1, n))
 stack_obs = inla.stack(
   data = df_mako_b$pa,
   A = list(1, A_mako),
-  effects = list(m = rep(1, mesh5$n),
+  effects = list(m = rep(1, n),
                  i = 1:spde$n.spde),
   tag = "mako_obs"
 )
@@ -157,33 +157,72 @@ stack = inla.stack(stack_obs, stack_system)
 
 # fomula = y ~ 
   
-  
-  
-  
-  
-  
-  
-n = 200
-loc = matrix(runif(n*2),n,2)
-mesh = inla.mesh.create.helper(points.domain=loc,
-                               max.edge=c(0.05, 0.2))
-proj.obs = inla.mesh.projector(mesh, loc=loc)
-proj.pred = inla.mesh.projector(mesh, loc=mesh$loc)
-spde = inla.spde2.matern(mesh,
-                         B.tau=cbind(log(1), 1, 0),
-                         B.kappa=matrix(c(log(sqrt(8)/0.2), 0, 1), 1, 3))
 
-covar = rnorm(n)
-field = inla.qsample(n=1, Q=inla.spde2.precision(spde, theta=c(0,0)))[,1]
-y = 2*covar + inla.mesh.project(proj.obs, field)
 
-A.obs = inla.spde.make.A(mesh, loc=loc)
-A.pred = inla.spde.make.A(mesh, loc=proj.pred$loc)
-dim(A.obs)
-stack.obs =
-  inla.stack(data=list(y=y),
-             A=list(A.obs, 1),
-             effects=list(c(list(intercept=rep(1, mesh$n)),
-                            inla.spde.make.index("spatial", spde$n.spde)),
-                          covar=covar),
-             tag="obs")
+data("SPDEtoy")
+
+pl.dom <- cbind(c(0, 1, 1, 0.7, 0), c(0, 0, 0.7, 1, 1))
+mesh5 <- inla.mesh.2d(loc.domain = pl.dom, max.e = c(0.092, 0.2))
+plot(mesh5)
+
+spde5 <- inla.spde2.pcmatern(
+  # Mesh and smoothness parameter 
+  mesh = mesh5, alpha = 2,
+  # P(practic.range < 0.3) = 0.5 
+  prior.range = c(0.3, 0.5),
+  # P(sigma > 1) = 0.01 
+  prior.sigma = c(10, 0.01))
+
+coords <- as.matrix(SPDEtoy[, 1:2])
+A5 <- inla.spde.make.A(mesh5, loc = coords)
+dim(A5)
+table(rowSums(A5 > 0))
+table(rowSums(A5))
+table(colSums(A5) > 0)
+table(as.numeric(A5))
+A1 <- inla.spde.make.A(mesh1, loc = coords)
+dim(A1)
+table(rowSums(A1 > 0))
+table(rowSums(A1))
+table(colSums(A1) > 0)
+table(as.numeric(A1))
+
+stk5 <- inla.stack(
+  data = list(resp = SPDEtoy$y),
+  A = list(A5, 1),
+  effects = list(i = 1:spde5$n.spde,
+                 beta0 = rep(1, nrow(SPDEtoy))), 
+  tag = 'est')
+
+
+
+
+  
+  
+  
+  
+  
+# n = 200
+# loc = matrix(runif(n*2),n,2)
+# mesh = inla.mesh.create.helper(points.domain=loc,
+#                                max.edge=c(0.05, 0.2))
+# proj.obs = inla.mesh.projector(mesh, loc=loc)
+# proj.pred = inla.mesh.projector(mesh, loc=mesh$loc)
+# spde = inla.spde2.matern(mesh,
+#                          B.tau=cbind(log(1), 1, 0),
+#                          B.kappa=matrix(c(log(sqrt(8)/0.2), 0, 1), 1, 3))
+# 
+# covar = rnorm(n)
+# field = inla.qsample(n=1, Q=inla.spde2.precision(spde, theta=c(0,0)))[,1]
+# y = 2*covar + inla.mesh.project(proj.obs, field)
+# 
+# A.obs = inla.spde.make.A(mesh, loc=loc)
+# A.pred = inla.spde.make.A(mesh, loc=proj.pred$loc)
+# dim(A.obs)
+# stack.obs =
+#   inla.stack(data=list(y=y),
+#              A=list(A.obs, 1),
+#              effects=list(c(list(intercept=rep(1, mesh$n)),
+#                             inla.spde.make.index("spatial", spde$n.spde)),
+#                           covar=covar),
+#              tag="obs")
