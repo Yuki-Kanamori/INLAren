94dd82d787f5a59a37637b4c70048ea7fc2895b8
Yuki-Kanamori <y.kanamory@gmail.com>
2020-10-06 08:20:49 +0900

    ?

--- joint.R
+++ joint.R
@@ -138,3 +138,37 @@ plot(pred_mean)
 
 
 
+
+# test for prediction (from inlabru) ----------------------------
+library(INLA) 
+library(inlabru) 
+library(RColorBrewer) 
+data(gorillas)
+gnests <- gorillas$nests
+mesh <- gorillas$mesh
+boundary <- gorillas$boundary
+gcounts <- gorillas$plotsample$counts 
+plots <- gorillas$plotsample$plots 
+plotnests <- gorillas$plotsample$nests
+
+fig.plots <- ggplot() +
+  gg(boundary) +
+  gg(plots) +
+  gg(gnests, size=0.07,color = "blue") +
+  gg(plotnests,size=0.07,color = "red") + geom_label(aes(label=gcounts$count, x=gcounts$x+0.35, y=gcounts$y-0.3), fill = "white", size = 1.5, label.padding = unit(0.1, "lines")) + coord_fixed() + xlab("x") + ylab("y")
+fig.plots
+
+mdl <- count ~ spat(map = coordinates, model = inla.spde2.matern(mesh, constr = FALSE)) + Intercept
+fit = bru(mdl, family = "poisson", data = gcounts, options = list(E = gcounts$exposure))
+
+pxl <- pixels(mesh, mask = boundary, nx = 500, ny = 400)
+dens <- predict(fit, pxl, ~ exp(spat+Intercept))
+
+pxl2 <- pixels(mesh2, mask = NULL, nx = 50, ny = 50)
+dens2 <- predict(fit, pxl, ~ exp(spat+Intercept))
+
+x = 150; y = 150
+lattice <- INLA::inla.mesh.lattice(x = x, y = y)
+
+
+pxl <- pixels(, nx = 50, ny = 50)
